05cec2a5f5bfb6e8c9ce429e6d8743b3e5cfbf34
CodeZapper <gabriele.nazeri@tiscali.it>
2018-04-28 21:12:20 +0200

    Added button to open until connected

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -10,20 +10,29 @@ public class Percolation {
         initUI();
     }
 
+    private void openUntilConnected(Surface surface) {
+        while (surface.findRoot(surface.virtual_top) != surface.findRoot(surface.virtual_bottom)) {
+            surface.open(new Random().nextInt(50), new Random().nextInt(50));
+        }
+    }
+
     private void initUI() {
         JFrame frame = new JFrame("CustomLayoutDemo");
         Container pane = frame.getContentPane();
 
         pane.setLayout(new BorderLayout());
         JPanel btnPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JButton btnOpenUntilConnected = new JButton("Open until connected");
         JButton btnOpen = new JButton("Open random site");
         JButton btnQuit = new JButton("Quit");
 
+        btnPanel.add(btnOpenUntilConnected);
         btnPanel.add(btnOpen);
         btnPanel.add(btnQuit);
         JPanel topPanel = new JPanel();
         JPanel mainPanel = new JPanel(new BorderLayout());
 
+        btnOpenUntilConnected.addActionListener(e -> openUntilConnected(surface));
         btnOpen.addActionListener(e -> surface.open(new Random().nextInt(50), new Random().nextInt(50)));
         btnQuit.addActionListener(e -> System.exit(0));
 
